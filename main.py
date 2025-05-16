a28366d22726c9b7b8e35e0f3796101ddf9b03ff
insomnial <akurt6805@protonmail.com>
2025-05-15 17:56:24 -0700

    Cachese the manifest, loads the file as a dictionary, creates a window.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from app.bungiemanifest import DestinyManifest
+import PySimpleGUI as sg
+import json
+import os
+import sys
+
 
 ###############################################################################
 #
@@ -8,3 +13,27 @@ from app.bungiemanifest import DestinyManifest
 if __name__ == '__main__':
     # check manifest
     manifest = DestinyManifest().update()
+    activityDict = {}
+    with open('./cache/DestinyActivityDefinition') as json_file:
+        activityDict = json.load(json_file)
+        json_file.close()
+    
+
+    layout = [
+        [sg.Text("Hello World")],
+        [sg.Button("OK"), sg.Button("Cancel")],
+    ]
+
+    window = sg.Window(title="Hello world", layout=layout, size=(300, 100), finalize=True)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Cancel":
+            break
+        elif event == "OK":
+            print("OK")
+        else:
+            print(event, values)
+    window.close()
+    
+    pass
